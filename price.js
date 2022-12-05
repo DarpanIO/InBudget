82d6d8af6d1b30c779bffad134710e4b52f7e4bb
darpan9460@gmail.com <darpan9460@gmail.com>
2022-12-06 03:04:55 +0530

    Added no. of days needeed to purchase product

--- price.js
+++ price.js
@@ -5,10 +5,26 @@ chrome.storage.local.get(["sallary"]).then((result) => {
   sallary = parseInt(result.sallary);
   console.log(result.sallary);
   for (let priceIndex in prices) {
-    let price = parseInt(prices[priceIndex].innerHTML.replace(",", ""));
+    let price = parseInt(
+      prices[priceIndex].innerHTML.includes(",")
+        ? prices[priceIndex].innerHTML.replaceAll(",", "")
+        : prices[priceIndex].innerHTML
+    );
+    let days = Math.ceil((price / (sallary / 30)) % 30);
+    let months = Math.floor(price / (sallary / 30) / 30);
     prices[priceIndex].innerHTML =
-      price < sallary
-        ? prices[priceIndex].innerHTML + "<button>In Budget</button>"
-        : prices[priceIndex].innerHTML + "<button>Out of Budget</button>";
+      months > 0
+        ? prices[priceIndex].innerHTML +
+          "<p class='month'>" +
+          months +
+          " Months " +
+          days +
+          " Days" +
+          "</p>"
+        : prices[priceIndex].innerHTML +
+          "<p class='days'>" +
+          days +
+          " Days" +
+          "</p>";
   }
 });
